3b14c7b435814e292cf463363ed9a820d877c733
madgine <marinabelogaz@gmail.com>
2025-02-09 19:57:43 +0300

    add prefab for menu buttons;

---
+++ Assets/Scripts/UI/Core/BaseUIButton.cs
@@ -0,0 +1,26 @@
+using Audio;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class BaseUIButton : MonoBehaviour
+{
+    private Button _button;
+    [Inject] private SoundManager _soundManager;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(OnButtonClicked);
+    }
+
+    private void OnButtonClicked()
+    {
+        _soundManager.PlaySoundByType(GameAudioType.ButtonCLick, 0, Vector3.zero);
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.AddListener(OnButtonClicked);
+    }
+}
