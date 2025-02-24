fb3113b2d199c43c41b648e5636d8c72fcbb2a4e
madgine <marinabelogaz@gmail.com>
2025-02-24 20:14:38 +0300

    fixed after merge;

--- Assets/Scripts/DI/RepositoryInstaller.cs
+++ Assets/Scripts/DI/RepositoryInstaller.cs
@@ -7,16 +7,10 @@ namespace Farm.DI
     [CreateAssetMenu(fileName = "Repository Installer", menuName = "Game Resources/Repository Installer")]
     public class RepositoryInstaller : ScriptableObjectInstaller
     {
-        [SerializeField] private EmbryoRepository _embryoRepository;
         [SerializeField] private TheOldOneRepository _theOldOneRepository;
 
         public override void InstallBindings()
         {
-            Container
-                .Bind<EmbryoRepository>()
-                .FromInstance(_embryoRepository)
-                .AsSingle();
-       
             Container
                 .Bind<TheOldOneRepository>()
                 .FromInstance(_theOldOneRepository)
